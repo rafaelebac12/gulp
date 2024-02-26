9ee595794d1488fe7c27b5bbe68e1d9f11565387
Rafael <rafael.arujo1800@gmail.com>
2024-02-26 13:15:10 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+
+function comprimeimagens() {
+    return gulp.src('./source/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'));
+}
+
+function comprimejavascript() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/scripts'))
+}
+
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./build/styles'));
+}
+
+exports.watch = function() {
+    gulp.watch('./source/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass));
+    gulp.watch('./source/scripts/*.js', {ignoreInitial: false}, gulp.series(comprimejavascript));
+    gulp.watch('./source/images/*', {ignoreInitial: false}, gulp.series(comprimeimagens));
+}
+
+
